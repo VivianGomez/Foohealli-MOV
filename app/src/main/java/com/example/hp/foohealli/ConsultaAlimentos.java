a96d11798417d23adca741d91ab81fc120a1990c
mf.mena <mf.mena@uniandes.edu.co>
2018-04-28 17:46:58 -0500

    AGUACATEEEE imagen

--- app/src/main/java/com/example/hp/foohealli/ConsultaAlimentos.java
+++ app/src/main/java/com/example/hp/foohealli/ConsultaAlimentos.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -21,6 +22,11 @@ public class ConsultaAlimentos extends AppCompatActivity {
         imagenAlimento= (ImageView) findViewById(R.id.fotoAlimento);
         mStorage= FirebaseStorage.getInstance().getReference();
         Uri link= Uri.parse("https://firebasestorage.googleapis.com/v0/b/prueba-c6bc7.appspot.com/o/Aguacate.jpg?alt=media&token=9c2d2bdb-b8a2-402a-9712-8a645c9320c5");
+        Glide.with(ConsultaAlimentos.this)
+        .load(link)
+        .fitCenter()
+        .centerCrop()
+        .into(imagenAlimento);
     }
 
 
