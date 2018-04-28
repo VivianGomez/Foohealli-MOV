24b8de1b4628130133327ff226068e86c290ab8a
mf.mena <mf.mena@uniandes.edu.co>
2018-04-28 17:27:01 -0500

    por si nos sacan de la sala

---
+++ app/src/main/java/com/example/hp/foohealli/ConsultaAlimentos.java
@@ -0,0 +1,27 @@
+package com.example.hp.foohealli;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+public class ConsultaAlimentos extends AppCompatActivity {
+
+    private ImageView imagenAlimento;
+    private StorageReference mStorage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_alimentos);
+        imagenAlimento= (ImageView) findViewById(R.id.fotoAlimento);
+        mStorage= FirebaseStorage.getInstance().getReference();
+        Uri link= Uri.parse("https://firebasestorage.googleapis.com/v0/b/prueba-c6bc7.appspot.com/o/Aguacate.jpg?alt=media&token=9c2d2bdb-b8a2-402a-9712-8a645c9320c5");
+    }
+
+
+}
