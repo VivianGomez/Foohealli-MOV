bab23d14ba4eea78b0883b211e322234b7240a31
kk.penaranda <kk.penaranda@uniandes.edu.co>
2018-04-28 13:02:35 -0500

    Cambios

---
+++ app/src/main/java/com/example/hp/foohealli/Examenes.java
@@ -0,0 +1,13 @@
+package com.example.hp.foohealli;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Examenes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examenes);
+    }
+}
