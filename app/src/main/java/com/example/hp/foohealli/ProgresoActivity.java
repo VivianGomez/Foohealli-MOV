4ec5de613ea5dbaf60c425352389f0cce72f488d
kkpenaranda <kkpenaranda@bitbucket.org>
2018-04-29 18:54:28 -0500

    Pantallas completas (Falta logica de back)

--- app/src/main/java/com/example/hp/foohealli/ProgresoActivity.java
+++ app/src/main/java/com/example/hp/foohealli/ProgresoActivity.java
@@ -1,7 +1,10 @@
 package com.example.hp.foohealli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgresoActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class ProgresoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progreso);
+
+        Button botonCuerpo = (Button) findViewById(R.id.button8);
+        botonCuerpo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pedirMiCuerpo();
+            }
+        });
+    }
+
+    private void pedirMiCuerpo(){
+        Intent pantalla =new Intent(this,MiCuerpoActivity.class);
+        startActivity(pantalla);
     }
 }
