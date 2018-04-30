d1d4600ef9592fe2084467d0561dfdf46274ea9d
mariafer mb <mf.mena@uniandes.edu.co>
2018-04-30 06:52:08 -0500

    Integracion de la consulta completa pero sin probar

--- app/src/main/java/com/example/hp/foohealli/ConsultaAlimentos.java
+++ app/src/main/java/com/example/hp/foohealli/ConsultaAlimentos.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.hp.foohealli.objetos.Alimento;
@@ -29,6 +30,8 @@ public class ConsultaAlimentos extends AppCompatActivity {
     private TextInputEditText busqueda;
     private Button buscar;
     private Uri foto;
+    private TextView campo;
+    private TextView valor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,6 @@ public class ConsultaAlimentos extends AppCompatActivity {
                 buscarEnLaBase();
             }
         });
-
 
         imagenAlimento= (ImageView) findViewById(R.id.fotoAlimento);
         mStorage= FirebaseStorage.getInstance().getReference();
@@ -72,6 +74,96 @@ public class ConsultaAlimentos extends AppCompatActivity {
                            .fitCenter()
                            .centerCrop()
                            .into(imagenAlimento);
+                   campo=(TextView) findViewById(R.id.CampoProteina);
+                   campo.setText("Proteina");
+                   valor=(TextView) findViewById(R.id.ValorProteina);
+                   valor.setText(alimento.getProteina()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoHidraCar);
+                   campo.setText("Hidratos de Carbono");
+                   valor=(TextView) findViewById(R.id.ValorHidraCar);
+                   valor.setText(alimento.getHidratosCarbono()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoFibra);
+                   campo.setText("Fibra");
+                   valor=(TextView) findViewById(R.id.ValorFibra);
+                   valor.setText(alimento.getFibra()+"");
+
+
+
+                   campo=(TextView) findViewById(R.id.CampoGrasaT);
+                   campo.setText("Gasas Totales");
+                   valor=(TextView) findViewById(R.id.ValorGrasaT);
+                   valor.setText(alimento.getGrasaTotal()+"");
+
+
+
+                   campo=(TextView) findViewById(R.id.CampoColesterol);
+                   campo.setText("Colesterol");
+                   valor=(TextView) findViewById(R.id.ValorColesterol);
+                   valor.setText(alimento.getColesterol()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoAlcohol);
+                   campo.setText("Alcohol");
+                   valor=(TextView) findViewById(R.id.ValorAlcohol);
+                   valor.setText(alimento.getAlcohol()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoAgua);
+                   campo.setText("Agua");
+                   valor=(TextView) findViewById(R.id.ValorAgua);
+                   valor.setText(alimento.getAgua()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoCalcio);
+                   campo.setText("Calcio");
+                   valor=(TextView) findViewById(R.id.ValorCalcio);
+                   valor.setText(alimento.getCalcio()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoHierro);
+                   campo.setText("Hierro");
+                   valor=(TextView) findViewById(R.id.ValorHierro);
+                   valor.setText(alimento.getHierro()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoSodio);
+                   campo.setText("Sodio");
+                   valor=(TextView) findViewById(R.id.ValorSodio);
+                   valor.setText(alimento.getSodio()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoPotasio);
+                   campo.setText("Potasio");
+                   valor=(TextView) findViewById(R.id.ValorPotasio);
+                   valor.setText(alimento.getPotasio()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoFosforo);
+                   campo.setText("Fosforo");
+                   valor=(TextView) findViewById(R.id.ValorFosforo);
+                   valor.setText(alimento.getFosforo()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoVitaminaD);
+                   campo.setText("VitaminaD");
+                   valor=(TextView) findViewById(R.id.ValorVitaminaD);
+                   valor.setText(alimento.getVitaminaD()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoVitaminaA);
+                   campo.setText("VitaminaA");
+                   valor=(TextView) findViewById(R.id.ValorVitaminaA);
+                   valor.setText(alimento.getVitaminaA()+"");
+
+
+                   campo=(TextView) findViewById(R.id.CampoVitaminaC);
+                   campo.setText("VitaminaC");
+                   valor=(TextView) findViewById(R.id.ValorVitaminaC);
+                   valor.setText(alimento.getVitaminaC()+"");
                }
 
                @Override
